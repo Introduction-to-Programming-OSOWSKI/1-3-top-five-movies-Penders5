c312df9d87e359c0997010cd061245e1b0916bd8
Penders5 <bigdlego.dp@gmail.com>
2021-09-21 09:33:57 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#defining topFiveMovies
+def topFiveMovies(a, b, c, d, e):
+
+    return a + " " + b + " " + c + " " + d + " " + e
+
+print(topFiveMovies("Transformers", "The Lego Movie", "Sonic", "The Fast And Furious", "Something else"))
